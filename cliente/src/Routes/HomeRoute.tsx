f9c7ef2ea3bee208868490066d8bb5e777c44825
MarcosInfnet <marcos.vpereira@al.infnet.edu.br>
2023-09-11 22:59:46 -0300

    Versão final

---
+++ cliente/src/Routes/HomeRoute.tsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import {api} from "../api";
+import {Toggle1} from "../components/Toggle1";
+import {Kudos} from "../components/Kudos";
+import { AiOutlineLoading3Quarters as LoadBar} from 'react-icons/Ai'
+import { Card } from "../components/Card";
+
+const initialNotepadsList = {
+    count: 0,
+    notepads:[],
+};
+const initialLoading = true;
+
+export function HomeRoute(){
+
+const [ notepadsList , setnotepadsList ] = useState(initialNotepadsList);
+const [ loading, setLoading] = useState(initialLoading);
+
+async function loadNotepads(){
+    const response = await api.get("/notepads");
+    const nextNotepads = response.data;
+    // console.log(response);
+    // console.log(nextNotepads);
+    setnotepadsList(nextNotepads);
+}
+
+    useEffect(() => {
+        loadNotepads();
+    },[ ]);
+
+    useEffect(() => {
+        if(notepadsList.notepads.length >0) {
+            setLoading(false);
+        }
+    },[notepadsList]);
+
+    const [checked,setChecked] = useState(false);
+
+    return <header className="flex flex-col justify-between max-w-screen-md md:mx-auto" >
+    
+    {loading &&
+    <div className="flex absolute py-7 top-18 left-5">
+        <LoadBar className="text-4xl animate-spin"/>
+    </div>}
+    
+    <div className="flex absolute py-7 top-18 right-5">
+    <Toggle1 checked={checked} onClick={()=> setChecked(true)}/>
+    </div>
+    
+    <Card className ={`p-1 bg-amber-100 my-1 md:max-w-screen-md md:mx-auto rounded-lg shadow flex relative   ${checked ? 'flex-row text-green-400 w-full flex-wrap gap-x-96':'py-10  grid grid-cols-3 gap-2 place-content-start w-full'}`}>
+        
+        {notepadsList.notepads.map(notepad => {
+        
+        return (
+
+        <div className={`flex relative flex-row flex-wrap mx-2 ${checked ? 'flex-row mx-4 ':'flex-col'}`}>
+            
+            <div key={notepad.id} className="p-2 bg-amber-100 my-2 md:max-w-screen-md md:mx-auto rounded-lg shadow h-auto">
+                
+                <span className="text-gray-500 mb-1 flex flex-wrap">#{notepad.id}</span>
+                <span className="text-md text-gray-500 leading-tight mb-2">{new Date(notepad.createdAt).toLocaleDateString()}</span>
+                <p className='text-md leading-tight mb-1'>{notepad.creator}</p> 
+                <h2 className='text-lg font-bold leading-tight mb-1'>{notepad.title}</h2>
+                <p className='text-md leading-tight mb-1'>{notepad.subtitle}</p>
+                <p className='text-md leading-tight mb-5 overflow-hidden truncate w-48'>{notepad.content}</p>
+                <div className="flex justify-end">
+                <Kudos className="flex content-end" to={`${notepad.id}`}/>
+                </div>   
+            </div>
+        </div>
+       )})}
+
+    </Card>
+    </header>
+
+    }
